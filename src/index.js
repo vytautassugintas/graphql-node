8b5267bd9b4666ac6d4579591d87dfee0500e762
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-06-18 19:52:23 +0300

    added custom link type resolver

--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+},
+{
+  id: 'link-1',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernew Clone`
+    info: () => `This is the API of a Hackernew Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url
   }
 }
 
